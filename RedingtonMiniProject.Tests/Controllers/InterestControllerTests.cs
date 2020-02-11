f8d9534a198a8d2f16094aaf2d4a9b3738710a8a
Marcin Konrad Ceglarek <marcin.ceglarek@redington.co.uk>
2020-02-11 20:48:18 +0100

    Added swagger and interest controller

---
+++ RedingtonMiniProject.Tests/Controllers/InterestControllerTests.cs
@@ -0,0 +1,57 @@
+﻿namespace RedingtonMiniProject.Tests.Controllers
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using RedingtonMiniProject.Controllers;
+
+    [TestClass]
+    public class InterestControllerTests
+    {
+        [TestMethod]
+        public void CalculateInterest1_TwoYears()
+        {
+            var startDate = new DateTime(2020, 01, 01);
+            var endDate = new DateTime(2022, 01, 01);
+            var interest = 0.05;
+
+            var ctrl = new InterestController();
+
+            // Act
+            var result = ctrl.CalculateInterest1(startDate, endDate, interest);
+
+            Assert.AreEqual(0.1025, result);
+        }
+
+        [TestMethod]
+        public void CalculateInterest2()
+        {
+            var startDate = new DateTime(2020, 01, 01);
+            var endDate = new DateTime(2022, 01, 01);
+            var interest = 0.05;
+
+            var ctrl = new InterestController();
+
+            // Act
+            var result = ctrl.CalculateInterest2(startDate, endDate, interest);
+
+            Assert.AreEqual(0.1025, result);
+        }
+
+        [TestMethod]
+        public void CalculateInterest3()
+        {
+            var startDate = new DateTime(2020, 01, 01);
+            var endDate = new DateTime(2022, 01, 01);
+            var interest = 0.05;
+
+            var ctrl = new InterestController();
+
+            // Act
+            var result = ctrl.CalculateInterest3(startDate, endDate, interest);
+
+            Assert.AreEqual(0.1025, result);
+        }
+    }
+}
---
+++ RedingtonMiniProject/Controllers/InterestController.cs
@@ -0,0 +1,37 @@
+﻿namespace RedingtonMiniProject.Controllers
+{
+    using System;
+    using System.Linq;
+    using System.Web.Http;
+
+    using WebGrease.Css.Extensions;
+
+    public class InterestController : ApiController
+    {
+        const double daysInYear = 365;
+
+        [HttpGet]
+        public double CalculateInterest1(DateTime startDate, DateTime endDate, double yearlyInterestRate)
+        {
+            var difference = endDate.Year - startDate.Year;
+            return yearlyInterestRate * difference;
+        }
+
+        [HttpGet]
+        public double CalculateInterest2(DateTime startDate, DateTime endDate, double yearlyInterestRate)
+        {
+            var difference = new DateTime(endDate.Year, endDate.Month, endDate.Day) - new DateTime(startDate.Year, startDate.Month, startDate.Day);
+            return Math.Pow(1 + (yearlyInterestRate / daysInYear), difference.TotalDays) - 1;
+        }
+
+        [HttpGet]
+        public double CalculateInterest3(DateTime startDate, DateTime endDate, double yearlyInterestRate)
+        {
+            var difference = endDate - startDate;
+            var dailyInterest = 1 + (yearlyInterestRate / daysInYear);
+            double result = 1.0;
+            Enumerable.Range(0, (int)difference.TotalDays).ForEach(i => result = result * dailyInterest);
+            return result - 1;
+        }
+    }
+}
